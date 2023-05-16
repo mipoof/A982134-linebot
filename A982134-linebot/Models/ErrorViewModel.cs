b8ad133540e2f59394e876c53f1221ca220d4563
mipoof <kiki.ket.key@gmail.com>
2023-05-16 15:31:34 +0800

    init

---
+++ A982134-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A982134_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
